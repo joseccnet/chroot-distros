09d40b62bd556fdeffb8b346814cacadfd475a9a
joseccnet <joseccnet@hotmail.com>
2015-01-04 22:48:48 -0600

    Actualizacion
    Actualizacion

--- update_chroot-distros.sh
+++ update_chroot-distros.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #Ejecute para revisar si hay actualizaciones o mejoras en los archivos del proyecto
-git rebase master
+git fetch --all
+git reset --hard origin/master
+git pull --rebase
+chmod 750 *.sh
