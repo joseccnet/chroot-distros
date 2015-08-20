df9ff649f442ba99dbdeece771a257dd5f8f69a7
joseccnet <joseccnet@hotmail.com>
2015-08-20 16:12:58 -0500

    small update

--- build-chroot-Ubuntu.sh
+++ build-chroot-Ubuntu.sh
@@ -40,6 +40,7 @@ version=$2
 arch=$3
 if [ "$arch" == "" ] ; then arch=$(uname -m); fi
 if [ "$arch" == "x86_64" ] ; then arch="amd64"; fi
+if [ "$arch" == "amd64" ] ; then paquetesadiocionalesDeb="$paquetesadiocionalesDeb,libc6-i386"; fi
 if [ "$version" == "" ] ; then version="vivid"; fi
 echo "Instalando..."
 echo -e "VERSION: $version \t ARCH: $arch"
